b1943346023522fa4e7f4c6e8012a5f8b7ff5825
daint1 <daint@vnpay.vn>
2024-06-07 17:06:11 +0700

    update: "hello jenkins with Jenkins file"

--- src/main/java/com/jenkins/hello_jenkins/HelloJenkinsApplication.java
+++ src/main/java/com/jenkins/hello_jenkins/HelloJenkinsApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloJenkinsApplication {
 
 	public static void main(String[] args) {
-		log.info("Hello Jenkins ver 2!");
+		log.info("Hello Jenkins with Jenkins file!");
 		SpringApplication.run(HelloJenkinsApplication.class, args);
 	}
 
